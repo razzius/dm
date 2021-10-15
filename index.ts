6389d45cb81ab855e6961e21345f602813da2d02
Razzi Abuissa <razzi@abuissa.net>
2021-10-15 12:45:09 -0400

    dm. version 0.1.0

---
+++ index.ts
@@ -0,0 +1,8 @@
+const version = '0.1.0'
+const messages = [{'from': '+13195943124', 'text': 'hi world'}]
+console.log(`dm. version ${version}\n`)
+console.log('Unread messages\n')
+for (let message of messages) {
+  console.log(`From: ${message.from}`)
+  console.log(`Message: ${message.text}\n`)
+}
