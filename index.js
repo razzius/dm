a7b6d01dac6dd32649f4c398a021ddf25dffac33
Jian Xie <Jianxie6464@gmail.com>
2021-10-26 15:48:22 -0700

    Ignore file add, Add new message to messages

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const version = '0.1.0'
-const messages = [{'from': '+13195943124', 'text': 'hi world'}]
+const messages = [{'from': '+13195943124', 'text': 'hi world'},{'from': '+13195943124', 'text':'Hello Library'}]
 console.log(`dm. version ${version}\n`)
 console.log('Unread messages\n')
 for (let message of messages) {
